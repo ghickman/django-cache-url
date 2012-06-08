ea65e451efe01a5fb739a816bb1dfc18bf4d2e36
George Hickman <george@ghickman.co.uk>
2012-06-08 22:29:40 +0100

    Rewrite the tests to have less asserts per test

--- tests.py
+++ tests.py
@@ -6,50 +6,96 @@ from nose.tools import assert_equals
 import django_cache_url
 
 
-DB = 'django.core.cache.backends.db.DatabaseCache'
-DUMMY = 'django.core.cache.backends.dummy.DummyCache'
-FILE = 'django.core.cache.backends.filebased.FileBasedCache'
-LOCMEM = 'django.core.cache.backends.locmem.LocMemCache'
-MEMCACHED = 'django.core.cache.backends.memcached.PyLibMCCache'
+class Base(TestCase):
+    def setUp(self):
+        try:
+            del environ['CACHE_URL']  # make sure
+        except KeyError:
+            pass
 
-class TestDjangoCacheUrl(TestCase):
-    def test_config_defaults_to_locmem(self):
-        config = django_cache_url.config()
-        assert_equals(config['BACKEND'], LOCMEM)
 
-    def test_db_url_returns_database_cache(self):
-        environ['CACHE_URL'] = 'db:///super_caching_table'
-        config = django_cache_url.config()
-        assert_equals(config['BACKEND'], DB)
-        assert_equals(config['LOCATION'], 'super_caching_table')
-
-    def test_dummy_url_returns_dummy_cache(self):
-        environ['CACHE_URL'] = 'dummy://'
-        config = django_cache_url.config()
-        print config
-        assert_equals(config['BACKEND'], DUMMY)
-        assert_equals(config['LOCATION'], '')
-
-    def test_file_url_returns_file_cache(self):
-        environ['CACHE_URL'] = 'file:///herp'
-        config = django_cache_url.config()
-        assert_equals(config['BACKEND'], FILE)
-        assert_equals(config['LOCATION'], 'herp')
-
-    def test_memcached_url_returns_pylibmc_cache(self):
-        environ['CACHE_URL'] = 'memcached://127.0.0.1:11211'
-        config = django_cache_url.config()
-        assert_equals(config['BACKEND'], MEMCACHED)
-        assert_equals(config['LOCATION'], '127.0.0.1:11211')
+class TestConfigOptions(Base):
+    location = 'django.core.cache.backends.memcached.PyLibMCCache'
 
     def test_setting_default_var(self):
-        config = django_cache_url.config(default='memcache://127.0.0.1:11211')
-        assert_equals(config['BACKEND'], MEMCACHED)
+        config = django_cache_url.config(default='memcached://127.0.0.1:11211')
+        assert_equals(config['BACKEND'], self.location)
         assert_equals(config['LOCATION'], '127.0.0.1:11211')
 
     def test_setting_env_var_name(self):
         environ['HERP'] = 'memcached://127.0.0.1:11211'
         config = django_cache_url.config(env='HERP')
-        assert_equals(config['BACKEND'], MEMCACHED)
+        assert_equals(config['BACKEND'], self.location)
         assert_equals(config['LOCATION'], '127.0.0.1:11211')
+
+
+class TestDatabaseCache(Base):
+    def setUp(self):
+        super(TestDatabaseCache, self).setUp()
+        environ['CACHE_URL'] = 'db://super_caching_table'
+
+    def test_db_url_returns_database_cache(self):
+        location = 'django.core.cache.backends.db.DatabaseCache'
+        config = django_cache_url.config()
+        assert_equals(config['BACKEND'], location)
+
+    def test_db_url_returns_location_from_url(self):
+        config = django_cache_url.config()
+        assert_equals(config['LOCATION'], 'super_caching_table')
+
+
+class TestDummyCache(Base):
+    def test_dummy_url_returns_dummy_cache(self):
+        environ['CACHE_URL'] = 'dummy://'
+        location = 'django.core.cache.backends.dummy.DummyCache'
+        config = django_cache_url.config()
+        assert_equals(config['BACKEND'], location)
+
+
+class TestFileCache(Base):
+    def setUp(self):
+        super(TestFileCache, self).setUp()
+        environ['CACHE_URL'] = 'file:///herp'
+
+    def test_file_url_returns_file_cache_backend(self):
+        location = 'django.core.cache.backends.filebased.FileBasedCache'
+        config = django_cache_url.config()
+        assert_equals(config['BACKEND'], location)
+
+    def test_file_url_returns_location_from_url(self):
+        config = django_cache_url.config()
+        assert_equals(config['LOCATION'], '/herp')
+
+
+class TestLocMemCache(Base):
+    location = 'django.core.cache.backends.locmem.LocMemCache'
+
+    def test_config_defaults_to_locmem(self):
+        config = django_cache_url.config()
+        assert_equals(config['BACKEND'], self.location)
+
+    def test_locmem_url_returns_locmem_cache(self):
+        environ['CACHE_URl'] = 'locmem://'
+        config = django_cache_url.config('')
+        assert_equals(config['BACKEND'], self.location)
+
+
+class TestMemcachedCache(Base):
+    def setUp(self):
+        super(TestMemcachedCache, self).setUp()
+        environ['CACHE_URL'] = 'memcached://127.0.0.1:11211/prefix'
+
+    def test_memcached_url_returns_pylibmc_cache(self):
+        location = 'django.core.cache.backends.memcached.PyLibMCCache'
+        config = django_cache_url.config()
+        assert_equals(config['BACKEND'], location)
+
+    def test_memcached_url_returns_location_from_url(self):
+        config = django_cache_url.config()
+        assert_equals(config['LOCATION'], '127.0.0.1:11211')
+
+    def test_memcached_url_returns_prefix_from_url(self):
+        config = django_cache_url.config()
+        assert_equals(config['PREFIX'], 'prefix')
+
 
