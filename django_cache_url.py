f873c1b54257d4d19ea05bc776de7147c8b3914d
Max Peterson <max@incuna.com>
2012-08-07 17:13:01 +0200

    Update django_cache_url.py
    Added pymemcached: django.core.cache.backends.memcached.MemcachedCache

--- django_cache_url.py
+++ django_cache_url.py
@@ -17,7 +17,8 @@ CACHE_TYPES = {
     'dummy': 'django.core.cache.backends.dummy.DummyCache',
     'file': 'django.core.cache.backends.filebased.FileBasedCache',
     'locmem': 'django.core.cache.backends.locmem.LocMemCache',
-    'memcached': 'django.core.cache.backends.memcached.PyLibMCCache'
+    'memcached': 'django.core.cache.backends.memcached.PyLibMCCache',
+    'pymemcached': 'django.core.cache.backends.memcached.MemcachedCache'
 }
 
 def config(env=DEFAULT_ENV, default='locmem://'):
