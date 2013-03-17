3969513a0086dbccc170227276224b741b7571ca
George Hickman <george@ghickman.co.uk>
2013-03-17 09:13:50 -0700

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup
 
 setup(
     name = 'django-cache-url',
-    version = '0.3.3',
+    version = '0.3.4',
     url = 'http://github.com/ghickman/django-cache-url',
     license = 'MIT',
     author = 'George Hickman',
